94ec59108a089dc6d06a59b85cdc8a0b5cedd3c0
abdninesix <mohabd1995@gmail.com>
2025-02-18 19:07:48 +0500

    Updated

--- src/app/Portfolio/page.jsx
+++ src/app/Portfolio/page.jsx
@@ -11,10 +11,9 @@ const PortfolioPage = () => {
   return (
     <motion.div className="h-full overflow-x-hidden  scrollbar scrollbar-track-transparent scrollbar-thumb-mytheme " initial={{ y: "-200vh" }} animate={{ y: "0%" }} transition={{ duration: 1 }}>
 
-      <div className="w-screen h-[calc(100vh-6rem)] flex flex-col gap-5 items-center justify-center text-2xl md:text-6xl text-center">
-        <p>Deployed project demos coming soon.</p>
+      <div className="w-screen h-[calc(100vh-6rem)] flex flex-col gap-4 items-center justify-center text-2xl md:text-6xl text-center">
+        <p>Project demos coming soon.</p>
         <p>Check GitHub for projects.</p>
-        <Arrow />
       </div>
 
     </motion.div>
