0025f1cea25b37263c2662913355a5c6bb068c7c
abdninesix <mohabd1995@gmail.com>
2025-04-22 12:26:52 +0500

    Updated

--- src/components/Slides.jsx
+++ src/components/Slides.jsx
@@ -2,7 +2,7 @@ export const projects = [
     [
         {
             src: "/projects/blog.png", alt: "blog",
-            title: "Blog website",
+            title: "Blog",
             desc: "A sleek and responsive fullstack blog website built with React, featuring a clean, modern UI. Users can read, create, and manage blog posts with smooth navigation and real-time updates. The site is optimized for performance and mobile-friendly, offering a seamless user experience across all devices.",
             link: "https://blog-by-abdullah.vercel.app/"
         },
@@ -11,6 +11,12 @@ export const projects = [
             title: "Minimal ToDo",
             desc: "A simple yet beautifully designed todo app built with a clean and intuitive interface. Users can easily add, update, and delete tasks, helping them stay organized and focused. The app offers a smooth user experience with responsive design, making task management effortless on any device.",
             link: "https://todo-by-abdullah.vercel.app/"
+        },
+        {
+            src: "/projects/calc.png", alt: "todo",
+            title: "AI Powered Calculator",
+            desc: "A simple yet beautifully designed todo app built with a clean and intuitive interface. Users can easily add, update, and delete tasks, helping them stay organized and focused. The app offers a smooth user experience with responsive design, making task management effortless on any device.",
+            link: "https://ai-calculator-by-abdullah.vercel.app/"
         },
     ],
 ];
