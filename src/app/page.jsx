bdd6eea69df1acafaa83d143507f5d090f0831f8
abdninesix <mohabd1995@gmail.com>
2025-06-27 15:28:04 +0500

    Updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,11 @@ import { motion } from "framer-motion";
 import Link from "next/link";
 
 const Homepage = () => {
+
+  const handleRightClick = (event) => {
+    event.preventDefault();
+  };
+
   return (
 
     <motion.div className="flex flex-col gap-12 md:gap-20 h-full overflow-hidden" initial={{ y: "-200vh" }} animate={{ y: "0%" }} transition={{ duration: 1 }}>
@@ -14,7 +19,7 @@ const Homepage = () => {
       <div className="items-center flex flex-col gap-4 md:gap-10 lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-40">
 
         {/*IMAGE CONTAINER*/}
-        <motion.div className="group relative flex-none overflow-hidden size-52 lg:size-80 border-2 border-mytheme rounded-t-full select-none" initial={{ y: -50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.5, delay: 1, ease: "easeInOut" }}>
+        <motion.div onContextMenu={handleRightClick} className="group relative flex-none overflow-hidden size-52 lg:size-80 border-2 border-mytheme rounded-t-full select-none" initial={{ y: -50, opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.5, delay: 1, ease: "easeInOut" }}>
           <div className="group-active:opacity-0 absolute top-[35%] ml-[31.5%] flex gap-1 lg:gap-3 w-fit -rotate-[12deg] z-10 duration-500">
             <div className="h-3 w-5 lg:h-5 lg:w-7 rounded-b-full bg-mytheme" />
             <div className="h-3 w-5 lg:h-5 lg:w-7 rounded-b-full bg-mytheme" />
